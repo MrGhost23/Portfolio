d24e89ea346bc96c9dabeecc30ebc5a986c1ff02
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-11-11 05:50:21 +0200

    added navData with map

--- components/Nav.js
+++ components/Nav.js
@@ -1,4 +1,5 @@
 // icons
+import Link from "next/link";
 import {
   HiHome,
   HiUser,
@@ -6,28 +7,42 @@ import {
   HiRectangleGroup,
   HiChatBubbleBottomCenterText,
   HiEnvelope,
-} from 'react-icons/hi2';
+} from "react-icons/hi2";
 
 // nav data
 export const navData = [
-  { name: 'home', path: '/', icon: <HiHome /> },
-  { name: 'about', path: '/about', icon: <HiUser /> },
-  { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
-  { name: 'work', path: '/work', icon: <HiViewColumns /> },
+  { name: "home", path: "/", icon: <HiHome /> },
+  { name: "about", path: "/about", icon: <HiUser /> },
+  { name: "services", path: "/services", icon: <HiRectangleGroup /> },
+  { name: "work", path: "/work", icon: <HiViewColumns /> },
   {
-    name: 'testimonials',
-    path: '/testimonials',
+    name: "testimonials",
+    path: "/testimonials",
     icon: <HiChatBubbleBottomCenterText />,
   },
   {
-    name: 'contact',
-    path: '/contact',
+    name: "contact",
+    path: "/contact",
     icon: <HiEnvelope />,
   },
 ];
 
+import { useRouter } from "next/router";
+
 const Nav = () => {
-  return <nav>nav</nav>;
+  return (
+    <nav>
+      <div>
+        {navData.map((link, index) => {
+          return (
+            <Link key={link.name} href={link.path}>
+              {link.icon}
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
