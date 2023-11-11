92cc6cfe23314c5a14c9551df9df5445ae74778a
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-11-11 05:39:32 +0200

    Added Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,24 @@
-const Layout = () => {
-  return <div>Layout</div>;
+import { Sora } from "@next/font/google";
+
+const sora = Sora({
+  subsets: ["latin"],
+  variable: "--font-sora",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
+
+import Navbar from "./Nav";
+import Header from "./Header";
+import TopLeftImg from "./TopLeftImg";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="page">
+      <TopLeftImg />
+      <Navbar />
+      <Header />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
